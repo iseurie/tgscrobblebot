554b44f0b5d48843e6d890d8d27a65bcc8be3ad1
eurie <iseurie@gmail.com>
2017-05-22 18:23:50 -0400

    update todo

--- src/main.java
+++ src/main.java
@@ -3,7 +3,7 @@ package dev.eurie.tglfmbot;
 public class TglScrobbleBot {
   public void main(String[] args) {
     // TODO: implement command-line options
-    // AES key file path (or a key string) and db file
+    // AES and last.fm API key file paths (or strings)
 
   }
 }
